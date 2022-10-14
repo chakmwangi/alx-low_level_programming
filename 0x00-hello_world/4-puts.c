8ab661e5d9b1c5acb86f859041d1a535d0913e52
Super User <chakmwangi@gmail.com>
2022-10-14 15:18:36 -0700

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  *  *main- Serves as the entry point for the program
@@ -10,4 +9,5 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
+
 
