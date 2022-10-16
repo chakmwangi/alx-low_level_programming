e2878128e1231ff5fbc97c5b5974c732e487c9c1
Super User <chakmwangi@gmail.com>
2022-10-16 16:54:08 -0700

    task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- *main-program entry point.
- *Return:0 no error, non zero if error
- */
+ *  *main-program entry point.
+ *   *Return:0 no error, non zero if error
+ *    */
 int main(void)
 {
-	char i;
+		char i;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+			for (i = '0'; i <= '9'; i++)
+					{
+								putchar(i);
+									}
+				putchar('\n');
+					return (0);
 }
