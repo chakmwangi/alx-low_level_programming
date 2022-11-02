59ff6e1dc805f94566cbd359f0b8f4ebce23f800
Super User <chakmwangi@gmail.com>
2022-11-02 13:32:15 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,55 @@
+#include "main.h"
+
 /**
- *  * _strspn - a function that gets the
- *   *           length of a prexif substring
- *    *
- *     * @s: pointer to string input
- *      * @accept: substring prefix to look for
- *       *
- *        * Return: the number of bytes in the initial segment
- *        */
+ *
+ *  * _strspn - Entry point
+ *
+ *   * @s: input
+ *
+ *    * @accept: input
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
 
 unsigned int _strspn(char *s, char *accept)
+
 {
-		int i, j, f;
 
-			i = 0;
-				while (s[i] != '\0')
+		unsigned int n = 0;
+
+			int r;
+
+
+
+				while (*s)
+
 						{
-									j = 0;
-											f = 1; /flag status/
-														while (accept[j] != '\0')
-																	{
-																					if (s[i] == accept[j])
-																									{
-																														f = 0; /success/
-																																			break;
-																																	}
-																								j++;
-																										}
-													if (f == 1)
-																	break;
-															i++;
-																}
 
-					return (i);
+									for (r = 0; accept[r]; r++)
+
+												{
+
+																if (*s == accept[r])
+
+																				{
+
+																									n++;
+
+																													break;
+
+																																}
+
+																			else if (accept[r + 1] == '\0')
+
+																								return (n);
+
+																					}
+
+											s++;
+
+												}
+
+					return (n);
+
 }
